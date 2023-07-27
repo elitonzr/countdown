9b379da67c3650f91914f0f00ed931d4f931ae23
elitonzr <25571917+Elitonzr@users.noreply.github.com>
2023-07-26 22:12:47 -0300

    Adição de thema por localstorage

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,30 @@
+const background = document.querySelector('.background');
+const box = document.querySelector('.box-imagem');
 const mode = document.getElementById('click');
 
+
 mode.addEventListener('click', () => {
-    console.log('Click')
-    const background = document.querySelector('.background');
-    const box = document.querySelector('.box-imagem');
-    if (background.classList.contains('theme')) {
-        background.classList.remove('theme');
+    if (background.classList.contains('theme1')) {
+        background.classList.remove('theme1');
+        background.classList.add('theme2');
+        localStorage.setItem('theme', 'theme2')
         return;
     }
-    background.classList.add('theme');
+    background.classList.remove('theme2');
+    background.classList.add('theme1');
+    localStorage.setItem('theme', 'theme1')
 
 });
+
+function theme() {
+    let theme = localStorage.getItem('theme');
+    if (theme == 'theme2') {
+        background.classList.remove('theme1');
+        background.classList.add('theme2');
+        return;
+    }
+    background.classList.remove('theme2');
+    background.classList.add('theme1');
+}
+
+theme();
