d1de59355679f73802d91d2cce38f09fcf9e9744
elitonzr <25571917+Elitonzr@users.noreply.github.com>
2023-08-01 20:42:40 -0300

    Ajustes após contador zerar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const updateDate = () => {
     const img = document.querySelector('.box-imagem');
+    const txtElement = document.getElementById('text');
 
     let date_ini = new Date(); // Data de hoje.
     let date_end = new Date("2023-08-02T07:00:00-03:00"); // Data alvo.
@@ -9,27 +10,50 @@ const updateDate = () => {
     let horas = formatDigit(Math.floor((milissegundo / 3600) % 24));
     let dias = formatDigit(Math.floor(milissegundo / (60 * 60 * 24)));
 
-    // Segundos
-    seconds.innerHTML = `${segundos}`;
-    // Minutos
-    minutes.innerHTML = `${minutos}`
-    // Horas    
-    hours.innerHTML = `${horas}`;
-    // Dias
-    days.innerHTML = `${dias}`;
+    if (segundos >= 0) {
+        // Segundos
+        seconds.innerHTML = `${segundos}`;
+
+        // Minutos
+        minutes.innerHTML = `${minutos}`
+
+        // Horas    
+        hours.innerHTML = `${horas}`;
+
+        // Dias
+        days.innerHTML = `${dias}`;
+
+        setTimeout(() => {
+            updateDate();
+        }, 1000);
+
+    } else {
+        txtElement.innerHTML = "Ele Voltou!"
+        const appElement = document.getElementById('app');
+
+        const H1Element = document.createElement("H1");
+        H1Element.innerHTML = "Vai trabalhar!";
+        appElement.appendChild(H1Element);
+
+        const timerElement = document.getElementById('timer');
+        timerElement.parentNode.removeChild(timerElement);
+
+        // const h1 = document.createElement("H1");
+        // const textNode = document.createTextNode("Hello World");
+        // h1.appendChild(textNode);
+        // document.body.appendChild(h1);
+    }
 
     // Cria um loop
-    setTimeout(() => {
-        updateDate();
-    }, 1000);
 
-    if ((segundos % 2) > 0) {
-        if (img.classList.contains('inverter')) {
-            img.classList.remove('inverter');
-        } else {
-            img.classList.add('inverter');
-        }
+
+    // if ((segundos % 2) > 0) {
+    if (img.classList.contains('inverter')) {
+        img.classList.remove('inverter');
+    } else {
+        img.classList.add('inverter');
     }
+    // }
 };
 
 // Acrescenta zero na frente de numeros menores que 9.
